9959e0d3c434d679634436626b253759064fdea5
tgritsaev <timgri200@yandex.ru>
2024-10-17 18:28:37 +0300

    augms

--- pretrain.py
+++ pretrain.py
@@ -18,19 +18,25 @@ import torchvision.transforms.functional as F
 
 class PretrainDataset(Dataset):
     
-    def __init__(self, path):
+    def __init__(self, path, transforms_set):
+        super().__init__()
+        
         self.path = path
-        cut = None if not args.debug else 128
-        self.len = len(os.listdir(path)) if not args.debug else cut
-        angles = [0, 90, 180, 270]
+        self.cut = None if not args.debug else 128
+        self.len = len(os.listdir(path)) if not args.debug else self.cut
+        self.transforms_set = transforms_set
         # Load the dataset on Disk
+        self._reset_data()
+    
+    def _reset_data(self):
+        angles = [0, 90, 180, 270]
         tensor_imgs = []
         for i in range(self.len):
             with Image.open(f"{self.path}/{i + 1}.jpg") as img:
-                tensor_imgs.append(TRANSFORM(img))
+                tensor_imgs.append(self.transforms_set(img))
         self.rotated_tensor_imgs = [
             torch.stack([F.rotate(tensor_img, angle) for angle in angles])
-            for tensor_img in tensor_imgs[:cut]
+            for tensor_img in tensor_imgs[:self.cut]
         ]
         
     def __len__(self):
@@ -41,15 +47,20 @@ class PretrainDataset(Dataset):
     
 class FineTuningDataset(Dataset):
     
-    def __init__(self, path):
+    def __init__(self, path, transforms_set):
+        super().__init__()
         self.path = path
-        
+        self.transforms_set = transforms_set
+        self._reset_data()
+        # print(f"Fine-tuning dataset size: {self.len}")
+    
+    def _reset_data(self):
         targets = []
         tensor_imgs = []
         for i, class_name in enumerate(CLASS_NAMES):
-            for fname in os.listdir(f"{path}/{class_name}"):
+            for fname in os.listdir(f"{self.path}/{class_name}"):
                 with Image.open(f"{self.path}/{class_name}/{fname}") as img:
-                    tensor_imgs.append(TRANSFORM(img))
+                    tensor_imgs.append(self.transforms_set(img))
                 targets.append(i)
         
         if args.debug:
@@ -63,8 +74,7 @@ class FineTuningDataset(Dataset):
         else:
             self.targets = targets
             self.tensor_imgs = tensor_imgs
-        self.len = len(self.targets)        
-        print(f"Fine-tuning dataset size: {self.len}")
+        self.len = len(self.targets)       
         
     def __len__(self):
         return self.len
@@ -99,7 +109,7 @@ parser.add_argument("--ft-epochs", default=300, type=int)
 parser.add_argument("-b", "--batch-size", default=128, type=int)
 parser.add_argument("-p", "--pt-learning-rate", default=0.1, type=float)
 parser.add_argument("--pt-gamma", default=0.95, type=float)
-parser.add_argument("-f", "--ft-learning-rate", default=1e-2, type=float)
+parser.add_argument("-f", "--ft-learning-rate", default=0.1, type=float)
 parser.add_argument("--ft-gamma", default=0.99, type=float)
 parser.add_argument("--weight-decay", default=5e-4, type=float)
 
@@ -109,18 +119,28 @@ parser.add_argument("-c", "--cpu-only", action="store_true")
 args = parser.parse_args()
 
 CLASS_NAMES = sorted(os.listdir("data/train/labeled"))
+print(f"class names: {CLASS_NAMES}, len: {len(CLASS_NAMES)}")
 mean_stats = [0.485, 0.456, 0.406]
 std_stats = [0.229, 0.224, 0.225]
-TRANSFORM = transforms.Compose([
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0), ratio=(0.75, 1.33)),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.05),
+    transforms.RandomGrayscale(p=0.1),
+    transforms.ToTensor(),  # Converts image to PyTorch tensor with values in [0, 1]
+    transforms.Normalize(mean=mean_stats, std=std_stats),  # Normalize the tensor,
+    transforms.RandomErasing(p=0.3, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+])
+test_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),  # Converts image to PyTorch tensor with values in [0, 1]
     transforms.Normalize(mean=mean_stats, std=std_stats)  # Normalize the tensor
 ])
-DENORMALIZE_TRANSFORM = transforms.Normalize(
+denormalize_transform = transforms.Normalize(
     mean=[-m/s for m, s in zip(mean_stats, std_stats)],  # Subtract mean / std
     std=[1/s for s in std_stats]                   # Divide by std
 )
 
-run_name = f"bs={args.batch_size}-ptlr={args.pt_learning_rate}-g={args.pt_gamma}-ftlr={args.ft_learning_rate}-g={args.ft_gamma}"
+run_name = f"AUGM--bs={args.batch_size}-ptlr={args.pt_learning_rate}-g={args.pt_gamma}-ftlr={args.ft_learning_rate}-g={args.ft_gamma}"
 logger = Logger(run_name)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() and not args.cpu_only else "cpu")
@@ -154,20 +174,22 @@ pt_optimizer = torch.optim.SGD(
     momentum=0.9,
     weight_decay=args.weight_decay,
 )
-print()
 pt_scheduler = torch.optim.lr_scheduler.ExponentialLR(pt_optimizer, gamma=args.pt_gamma)
 
-pt_dataset = PretrainDataset("data/train/unlabeled")
+pt_dataset = PretrainDataset("data/train/unlabeled", train_transforms)
 pt_dataloader = DataLoader(pt_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
-ft_dataset = FineTuningDataset("data/train/labeled")
-ft_dataloader = DataLoader(ft_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+ft_train_dataset = FineTuningDataset("data/train/labeled", train_transforms)
+ft_train_dataloader = DataLoader(ft_train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+
+ft_test_dataset = FineTuningDataset("data/train/labeled", test_transforms)
+ft_test_dataloader = DataLoader(ft_test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
 test_path = "data/test"
 test_images = []
 for i in range(len(os.listdir(test_path))):
     with Image.open(f"{test_path}/{i}.jpg") as img:
-        test_images.append(TRANSFORM(img))
+        test_images.append(test_transforms(img))
 
 pt_total_steps = 0
 
@@ -190,7 +212,7 @@ for epoch in trange(args.pretrain_epochs):
             logger.log({"train_loss": loss.item()}, pt_total_steps)
             
             log_images_n = 4
-            denormalized_images = [DENORMALIZE_TRANSFORM(img) for img in batch[:log_images_n]]
+            denormalized_images = [denormalize_transform(img) for img in batch[:log_images_n]]
             grid = make_grid(denormalized_images, nrow=log_images_n)
             grid_np = grid.permute(1, 2, 0).cpu().numpy()
             logger.log({"rotations": [wandb.Image(grid_np, caption=f"Predictions: {rotation_preds.argmax(1)[:log_images_n]}")]}, pt_total_steps, True) 
@@ -204,49 +226,51 @@ for epoch in trange(args.pretrain_epochs):
         }, 
         f"{save_dir}/pt-{epoch}.pt"
     )
+    pt_dataset._reset_data()
     
     is_final_epoch = epoch + 1 == args.pretrain_epochs
     for ftlr_multiplier in [1, 0.1, 0.01]:
-        ft_model = models.resnet18(num_classes=10)
+        ft_model = models.resnet18(num_classes=10).to(device)
         ft_model.load_state_dict(model.state_dict())
-        ft_model = ft_model.to(device)
+        # ft_model.fc.reset_parameters()
         ft_optimizer = torch.optim.SGD(ft_model.parameters(), lr=args.ft_learning_rate * ftlr_multiplier, momentum=0.9, weight_decay=args.weight_decay)
         ft_scheduler = torch.optim.lr_scheduler.ExponentialLR(ft_optimizer, gamma=args.ft_gamma)
-        for ft_epoch in range(args.ft_epochs if is_final_epoch else 1):
+        for ft_epoch in range(args.ft_epochs if is_final_epoch else 3):
             ft_model.train()
-            for (batch, target) in ft_dataloader:
+            for (batch, target) in ft_train_dataloader:
                 ft_optimizer.zero_grad()
                 
                 class_preds = ft_model(batch.to(device))
-                
                 loss = ce_loss(class_preds, target.to(device))
                 loss.backward()
                 ft_optimizer.step()
             if is_final_epoch:
                 logger.log({"final_ft_lr": ft_scheduler.get_last_lr()[0]}, ft_epoch)
             ft_scheduler.step()
+            ft_train_dataset._reset_data()
                 
             ft_model.eval()
             correct_pred_cnt = 0
             with torch.no_grad():
-                for (batch, target) in ft_dataloader:
+                for (batch, target) in ft_test_dataloader:
                     class_preds = ft_model(batch.to(device))
+
                     correct_pred_cnt += torch.sum(class_preds.argmax(-1) == target.to(device))
             
                 log_images_n = 8
-                denormalized_images = [DENORMALIZE_TRANSFORM(img) for img in batch[:log_images_n]]
+                denormalized_images = [denormalize_transform(img) for img in batch[:log_images_n]]
                 grid = make_grid(denormalized_images, nrow=log_images_n)
                 grid_np = grid.permute(1, 2, 0).cpu().numpy()
                 class_name_preds = [CLASS_NAMES[class_pred] for class_pred in class_preds.argmax(-1)[:log_images_n]]
                 if is_final_epoch:
                     logger.log({f"final_test_images_ftlrmul={ftlr_multiplier}": [wandb.Image(grid_np, caption=f"Predictions: {class_name_preds}")]}, ft_epoch, True)  
-                else:
+                elif ft_epoch == 2:
                     logger.log({f"test_images_ftlrmul={ftlr_multiplier}": [wandb.Image(grid_np, caption=f"Predictions: {class_name_preds}")]}, pt_total_steps, True)  
                 
             if is_final_epoch:
-                logger.log({f"final_ft_accuracy_ftlrmul={ftlr_multiplier}": correct_pred_cnt / len(ft_dataset)}, ft_epoch)
-            else:
-                logger.log({f"ft_accuracy_ftlrmul={ftlr_multiplier}": correct_pred_cnt / len(ft_dataset)}, pt_total_steps)
+                logger.log({f"final_ft_accuracy_ftlrmul={ftlr_multiplier}": correct_pred_cnt / len(ft_train_dataset)}, ft_epoch)
+            elif ft_epoch == 2:
+                logger.log({f"ft_accuracy_ftlrmul={ftlr_multiplier}": correct_pred_cnt / len(ft_train_dataset)}, pt_total_steps)
             
             test_preds = []
             with torch.no_grad():
